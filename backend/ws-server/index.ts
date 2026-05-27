2b31dc76ad681d4c2246b8585846aecba5edb7f5
LALIT666 <89864324+LALIT666@users.noreply.github.com>
2026-05-27 08:28:38 +0200

    types and yes one more thing right now i am making one file application

--- backend/ws-server/index.ts
+++ backend/ws-server/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello via Bun!");
+//mene bun add ws @types/ws redis kara okay
+
+import { WebSocket, WebSocketServer } from "ws";
+import { createClient, type RedisClientType } from "redis";
+
+// ========================
+// ! TYPES
+// ========================
+
+interface JoinMessage {
+  type: "join";
+  roomId: string;
+}
+
+interface ChatMessage {
+  type: "chat";
+  roomId: string;
+}
+
+interface RedisPayload {
+  roomId: string;
+  message: string;
+  senderId: string;
+}
+
+type IncomingMessage = JoinMessage | ChatMessage;
