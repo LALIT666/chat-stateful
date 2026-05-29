cac35bd23ed70d075bc4f690520de688f3567c24
LALIT666 <89864324+LALIT666@users.noreply.github.com>
2026-05-29 05:26:56 +0200

    ws server banaya and then generate id

--- backend/ws-server/index.ts
+++ backend/ws-server/index.ts
@@ -1,1 +1,24 @@
 // 2nd
+import { WebSocket, WebSocketServer } from "ws";
+
+const PORT = parseInt(process.env.PORT || "8080");
+
+const wss = new WebSocketServer({ port: PORT });
+
+function generateId() {
+  return Math.random().toString(36).substring(2, 10);
+}
+
+wss.on("connection", (socket: WebSocket) => {
+  const userId = generateId();
+  console.log(`user with userId: ${userId} is connected`);
+
+  socket.send(JSON.stringify({ type: "connected", userId }));
+
+  socket.on("message", (rawData: Buffer) => {
+    console.log("Got message: ", rawData.toString);
+  });
+  console.log("New client connected");
+});
+
+console.log(`WS Server is running on port: ${PORT}`);
